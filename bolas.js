907d526762c99ec91481690d6bcbb6ba94f527ba
Mocaszcin <50915726+Mocaszcin@users.noreply.github.com>
2019-05-22 14:38:50 -0300

    upload of p5js files

---
+++ bolas.js
@@ -0,0 +1,28 @@
+class Bolas {
+  constructor(posx, posy, radio, color) {
+    this.x = posx;
+    this.y = posy;
+    this.r = radio;
+    this.colores = color;
+  }
+
+  move() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 6);
+    if (this.x > width || this.x < 0) {
+      this.x = width / 2;
+    }
+    if (this.y > height || this.y < 0) {
+      this.y = random(0,height/10);
+    }
+
+  }
+
+  show() {
+    stroke(this.colores, 0 ,255-this.colores);
+    strokeWeight(3);
+    fill(255);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+let bolas = [];
+
+
+function setup() {
+  createCanvas(600, 400);
+
+
+}
+
+function draw() {
+  background(0);
+
+  for (let bola of bolas) {
+    bola.move();
+    bola.show();
+  }
+}
+
+function mouseDragged() {
+  let unidad=new Bolas(mouseX, mouseY, 5, random(0,255));
+  bolas.push(unidad)
+}
